a96d8978976381fdab3c8fa9a377092d56b7ff2b
Maissou <maissane.y@gmail.com>
2016-01-12 12:31:38 +0100

    Update vueInscription.php

--- Vue/vueInscription.php
+++ Vue/vueInscription.php
@@ -6,15 +6,15 @@
 </header>
 
 <form method="post" action="index.php?action=sinscrire">
-  Votre pseudo:  <input id="pseudo" name="pseudo" type="text" minlength="6" placeholder="Votre pseudo"><br/>
-  Votre mot de passe:  <input id="mdp" name="mdp" type="password" minlength="6" placeholder="Votre mot de passe"><br/>
-  Votre email:  <input id="email" name="email" type="email" placeholder="Votre email"><br/>
-  Votre nom:  <input id="nomp" name="nomp" type="text" minlength="2" placeholder="Votre nom"><br/>
-  Votre prenom:  <input id="prenomp" name="prenomp" type="text" minlength="3" placeholder="Votre prenom"><br/>
+  Votre pseudo:  <input id="pseudo" name="pseudo" type="text" minlength="6" placeholder="Votre pseudo" required><br/>
+  Votre mot de passe:  <input id="mdp" name="mdp" type="password" minlength="6" placeholder="Votre mot de passe" required><br/>
+  Votre email:  <input id="email" name="email" type="email" placeholder="Votre email" required><br/>
+  Votre nom:  <input id="nomp" name="nomp" type="text" minlength="2" placeholder="Votre nom" required><br/>
+  Votre prenom:  <input id="prenomp" name="prenomp" type="text" minlength="3" placeholder="Votre prenom" required><br/>
 
-  Votre adresse:  <input id="adresse" name="adresse" type="text" placeholder="Votre adresse"><br/>
-  Votre code postal:  <input id="cp" name="cp" minlength="5" type="text" placeholder="Votre Code postal"><br/>
-  Votre téléphone:  <input id="telephone" name="telephone" minlength="10" type="text" placeholder="Votre téléphone"><br/>
+  Votre adresse:  <input id="adresse" name="adresse" type="text" placeholder="Votre adresse" required><br/>
+  Votre code postal:  <input id="cp" name="cp" minlength="5" type="text" placeholder="Votre Code postal" required><br/>
+  Votre téléphone:  <input id="telephone" name="telephone" minlength="10" type="text" placeholder="Votre téléphone" required><br/>
 
   Votre avatar ou photos:  <input id="avatar" name="avatar" type="text" placeholder="Votre avatar"><br/>
 
